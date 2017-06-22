f0d16f5b519f0d4aa81840b7374f722e4919bcce
Jonathan Dang <dangydang9191@gmail.com>
2017-06-21 18:12:43 -0700

    Put a banana

--- index.android.js
+++ index.android.js
@@ -1,17 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native'
+import { AppRegistry, Image } from 'react-native';
 
-  export default class HelloWorldApp extends Component {
-    render() {
-     return (
-        <Text>KingJonathan</Text>
-      );
-   }
- }
-AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
